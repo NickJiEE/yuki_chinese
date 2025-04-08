ef9a30371586c6888e55da7751cd01796f112829
NickJiEE <yij036@ucsd.edu>
2025-04-07 18:07:13 -0700

    stuff

--- Yuki_chinese/main.py
+++ Yuki_chinese/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
     
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="docs")
 
 # Index page
 @app.get("/", response_class=HTMLResponse)
