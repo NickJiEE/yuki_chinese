c444947cc5e0525044441c927b8a4238607fa590
NickJiEE <yij036@ucsd.edu>
2025-04-09 23:04:48 -0700

    fix favicon

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ async def girl_four(request: Request):
 
 @app.get("/favicon.ico")
 def favicon():
-    return FileResponse("static/favicon.ico")
+    return FileResponse("/static/icons/favicon.ico")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
